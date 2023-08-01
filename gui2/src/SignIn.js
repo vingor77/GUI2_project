4f72e954afbe08e760d2aa7b088528432c102c5d
BenJCoding <124981289+BenJCoding@users.noreply.github.com>
2023-08-01 17:46:03 -0400

    Update SignIn.js
    Enables anonymous login

--- gui2/src/SignIn.js
+++ gui2/src/SignIn.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
+import { signInWithRedirect, GoogleAuthProvider, getAuth, signInAnonymously  } from "firebase/auth";
 
 import Landing from "./components/Landing";
 import "./style/App.css";
@@ -9,7 +9,18 @@ export default function SignIn(props) {
   const signInWithGoogle = () => {
     signInWithRedirect(props.auth, new GoogleAuthProvider());
   };
-
+  const signInWithAnon = () => {
+    const auth = getAuth();
+    signInAnonymously(auth)
+      .then(() => {
+        // Signed in..
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ...
+      });
+    };
   const imgStyle = {
     background: "#CAFFB9",
     position: "absolute",
@@ -19,10 +30,16 @@ export default function SignIn(props) {
     height: "auto",
   };
 
-  const signInButton = {
+  const signInButtonGoogle = {
     textAlign: "center",
     position: "absolute",
-    top: "50%",
+    top: "45%", // adjusted from 50%
+  };
+  
+  const signInButtonAnon = {
+    textAlign: "center",
+    position: "absolute",
+    top: "55%", // adjusted from 50%
   };
 
   const divStyle = {
@@ -38,11 +55,18 @@ export default function SignIn(props) {
       <Landing />
       {/* Sign-In Button */}
       <button
-        style={signInButton}
+        style={signInButtonGoogle}
         className=" offset-lg-5 col-lg-2 offset-3 col-6 sign-in"
         onClick={signInWithGoogle}
       >
         Sign in with Google
+      </button>
+      <button
+        style={signInButtonAnon}
+        className=" offset-lg-5 col-lg-2 offset-3 col-6 sign-in"
+        onClick={signInWithAnon}
+      >
+        Sign in anonymously
       </button>
       {/* Background Image */}
       <img style={imgStyle} src={background} alt="background community" />
